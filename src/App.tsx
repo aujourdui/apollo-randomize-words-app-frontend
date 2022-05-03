15b97573e402f937377fddaaefa2cb7eaf370d9d
ito <kyosuke.ito@gmail.com>
2022-05-03 14:04:44 -0700

    create basic words fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, FC } from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
+const App: FC = () => {
+  useEffect(() => {
+    axios
+      .get("https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf", {
+        headers: {
+          "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+          "X-RapidAPI-Key":
+            "e7bf698955msh0a7c2b723de2e5ep12bdd6jsnff909e419223",
+        },
+      })
+      .then((res) => console.log(res))
+      .catch((err) => {
+        console.error(new Error(err));
+      });
+  });
   return (
     <div className="App">
       <h1>Hello</h1>
     </div>
   );
-}
+};
 
 export default App;

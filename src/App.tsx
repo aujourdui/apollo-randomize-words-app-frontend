a5872a4967728ab73882243338b4f3259ab80f14
ito <kyosuke.ito@gmail.com>
2022-05-03 14:17:51 -0700

    change then to async

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,24 @@ import axios from "axios";
 import "./App.css";
 
 const App: FC = () => {
-  useEffect(() => {
-    axios
-      .get("https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf", {
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf",
+      {
         headers: {
           "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
           "X-RapidAPI-Key":
             "e7bf698955msh0a7c2b723de2e5ep12bdd6jsnff909e419223",
         },
-      })
-      .then((res) => console.log(res))
-      .catch((err) => {
-        console.error(new Error(err));
-      });
-  });
+      }
+    );
+    const words = await response.data;
+    console.log(words);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <h1>Hello</h1>
@@ -25,3 +29,18 @@ const App: FC = () => {
 };
 
 export default App;
+
+// useEffect(() => {
+//   axios
+//     .get("https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf", {
+//       headers: {
+//         "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+//         "X-RapidAPI-Key":
+//           "e7bf698955msh0a7c2b723de2e5ep12bdd6jsnff909e419223",
+//       },
+//     })
+//     .then((res) => console.log(res))
+//     .catch((err) => {
+//       console.error(new Error(err));
+//     });
+// });

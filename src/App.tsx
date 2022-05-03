e7fa5f475e0c83cd83a9818d8894b7738844c641
ito <kyosuke.ito@gmail.com>
2022-05-03 14:58:45 -0700

    create basic word fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,50 @@
-import React, { useEffect, FC } from "react";
+import React, { useState, useEffect, FC } from "react";
 import axios from "axios";
 import "./App.css";
 
+// interface IWords {
+//   typeOf: any;
+//   map: any;
+//   word: any;
+// }
+
 const App: FC = () => {
+  const [words, setWords] = useState<any>();
+
   const fetchData = async () => {
-    const response = await axios.get(
-      "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf",
-      {
-        headers: {
-          "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
-          "X-RapidAPI-Key":
-            "e7bf698955msh0a7c2b723de2e5ep12bdd6jsnff909e419223",
-        },
-      }
-    );
-    const words = await response.data;
-    console.log(words);
+    try {
+      const response = await axios.get(
+        "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf",
+        {
+          headers: {
+            "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+            "X-RapidAPI-Key":
+              "e7bf698955msh0a7c2b723de2e5ep12bdd6jsnff909e419223",
+          },
+        }
+      );
+      const data = await response.data;
+      setWords(data.word);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    console.log(words);
+  }, [words]);
   return (
     <div className="App">
       <h1>Hello</h1>
+      {words}
+      {/* <p>{words.word}</p> */}
+      {/* {words.map((word) => (
+        <li>{word.typeOf}</li>
+      ))} */}
     </div>
   );
 };

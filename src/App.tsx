c2adcb2b1114bf25a838f9a2a3bacdd68702d646
ito <kyosuke.ito@gmail.com>
2022-05-04 16:49:44 -0700

    finish basic function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, FC } from "react";
-import axios from "axios";
 import "./App.css";
 
 import { useQuery, gql, NetworkStatus } from "@apollo/client";
@@ -11,6 +10,7 @@ import {
   Button,
   ListItem,
   UnorderedList,
+  Textarea,
 } from "@chakra-ui/react";
 
 const GET_WORDS = gql`
@@ -41,6 +41,10 @@ const App: any = () => {
     refetch();
   };
 
+  const firstWord = data.words[randomNum()].word;
+
+  const secondWord = data.words[randomNum()].word;
+
   return (
     <div className="app">
       <Text fontSize="5xl" fontWeight="extrabold">
@@ -49,22 +53,23 @@ const App: any = () => {
       <>
         <UnorderedList>
           <Grid className="word__container">
-            <ListItem className="word__list--first">
-              {data.words[randomNum()].word}
-            </ListItem>
+            <ListItem className="word__list--first">{firstWord}</ListItem>
             <ListItem className="word__list--middle">X</ListItem>
-            <ListItem className="word__list--second">
-              {data.words[randomNum()].word}
-            </ListItem>
+            <ListItem className="word__list--second">{secondWord}</ListItem>
           </Grid>
         </UnorderedList>
         <Box className="refetch-button__container">
-          <Button onClick={() => refetch()}>Refetch</Button>
+          <Button onClick={handleRefetch}>Refetch</Button>
         </Box>
-        <Box className="refetch-button__container">
-          <Button onClick={handleRefetch}>
-            Create a new story with above words
-          </Button>
+        <Box>
+          <Text width="90%" fontSize="xl">
+            Create a new story
+          </Text>
+          <Textarea
+            width="300px"
+            size="lg"
+            defaultValue={[firstWord, secondWord]}
+          ></Textarea>
         </Box>
       </>
     </div>
@@ -72,88 +77,3 @@ const App: any = () => {
 };
 
 export default App;
-
-// import React, { useState, useEffect, FC } from "react";
-// import axios from "axios";
-
-// const App: FC = () => {
-//   const [words, setWords] = useState<any>();
-//   const [refetch, setRefetch] = useState<any>();
-
-//   const fetchData = async () => {
-//     try {
-//       const response = await axios.post("http://localhost:4000", {
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         query: `query{
-//             words {
-//               word
-//               type
-//             }
-//           }`,
-//       });
-//       const data = await response.data.data.words;
-//       setWords(data);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchData();
-//   }, [refetch]);
-
-//   const handleRefetch = () => {
-//     setRefetch(!refetch);
-//   };
-
-//   // useEffect(() => {
-//   //   console.log(words);
-//   // }, [words]);
-
-//   const randomNum = () => {
-//     return Math.floor(Math.random() * 12);
-//   };
-
-//   return (
-//     <div className="app">
-//       <Text fontSize="5xl" fontWeight="extrabold">
-//         Idea Booster
-//       </Text>
-//       {
-//         words && (
-//           <>
-//             <UnorderedList>
-//               <Grid className="word__container">
-//                 <ListItem className="word__list--first">
-//                   {words[randomNum()].word}
-//                 </ListItem>
-//                 <ListItem className="word__list--middle">X</ListItem>
-//                 <ListItem className="word__list--second">
-//                   {words[randomNum()].word}
-//                 </ListItem>
-//               </Grid>
-//             </UnorderedList>
-//             <Box className="refetch-button__container">
-//               <Button onClick={handleRefetch}>Refetch</Button>
-//             </Box>
-//             <Box className="refetch-button__container">
-//               <Button onClick={handleRefetch}>
-//                 Create a new story with above words
-//               </Button>
-//             </Box>
-//           </>
-//         )
-//         // words.map((word, index) => (
-//         //   <div key={index} className="word__container">
-//         //     <li className="word__list--first">{word.word}</li>
-//         //     <li className="word__list--second">{word.type}</li>
-//         //   </div>
-//         // ))
-//       }
-//     </div>
-//   );
-// };
-
-// export default App;

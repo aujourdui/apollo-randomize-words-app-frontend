570bd578a781c79349a72738d021f0ca35c3c855
ito <kyosuke.ito@gmail.com>
2022-05-05 11:03:24 -0700

    change to react version 17

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "App";
 import reportWebVitals from "reportWebVitals";
@@ -12,15 +13,16 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+ReactDOM.render(
   <ApolloProvider client={client}>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </ApolloProvider>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
